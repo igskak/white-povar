f9830776b9002b85d4b6dcc026efe81dbc3f01c2
Skak <skak.igor@gmail.com>
2025-08-15 17:23:17 +0200

    Fix duplicate builds by consolidating CI and deploy workflows

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:frontend/core/app.dart';
-
 void main() {
   testWidgets('App loads without crashing', (WidgetTester tester) async {
     // Build our app and trigger a frame.
